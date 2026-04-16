a46a173d4c6c784780607adfb1f54b1a392ffb76
R471QUL <raziqlgs7@gmail.com>
2026-04-16 10:44:55 +0700

    Implement leap year checker in soal3.cpp
    Add functionality to check leap years based on user input.

--- soal3.cpp
+++ soal3.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+using namespace std;
 
+bool kabisat(int tahun) {
+    if (tahun % 400 == 0) return true;
+    if (tahun % 4 == 0 && tahun % 100 != 0) return true;
+    return false;
+}
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    if (B >= 0) {
+        for (int i = 0; i <= B; i++) {
+            int tahun = A + i;
+            cout << tahun;
+
+            if (kabisat(tahun))
+                cout << " kabisat" << endl;
+            else
+                cout << " bukan kabisat" << endl;
+        }
+    } else {
+        for (int i = 0; i >= B; i--) {
+            int tahun = A + i;
+            cout << tahun;
+
+            if (kabisat(tahun))
+                cout << " kabisat" << endl;
+            else
+                cout << " bukan kabisat" << endl;
+        }
+    }
+
+    return 0;
+}
