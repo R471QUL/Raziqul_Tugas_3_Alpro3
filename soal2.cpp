78cd6d2a23a67e4f2b9434aec976d2ea6f238e9b
R471QUL <raziqlgs7@gmail.com>
2026-04-16 10:44:04 +0700

    Implement character removal from input string

--- soal2.cpp
+++ soal2.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    string kalimat;
+    char huruf;
+    int jumlah = 0;
+
+    getline(cin, kalimat);
+    cin >> huruf;
+
+    string hasil = "";
+
+    for (int i = 0; i < kalimat.length(); i++) {
+        if (kalimat[i] != huruf) {
+            hasil += kalimat[i];
+        } else {
+            jumlah++;
+        }
+    }
+
+    cout << hasil << endl;
+    cout << "Karakter terhapus = " << jumlah << endl;
+
+    return 0;
+}
