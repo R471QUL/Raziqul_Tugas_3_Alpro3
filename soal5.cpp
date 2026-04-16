55189da6d4328feaecd96115cd39867c7ab57f6d
R471QUL <raziqlgs7@gmail.com>
2026-04-16 22:07:56 +0700

    Implement prime number check in soal5.cpp

--- soal5.cpp
+++ soal5.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    bool isPrime = true;
+
+    cin >> n;
+
+    if(n <= 1) {
+        isPrime = false;
+    } else {
+        for(int i = 2; i <= n / 2; i++) {
+            if(n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    if(isPrime)
+        cout << "Prima";
+    else
+        cout << "Bukan Prima";
+}
