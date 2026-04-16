18307f7c199ff58ab804b8e7bd288a727b3c5f65
R471QUL <raziqlgs7@gmail.com>
2026-04-16 22:08:25 +0700

    Calculate sum of squares of even numbers up to n

--- soal6.cpp
+++ soal6.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    int total = 0;
+
+    cin >> n;
+
+    for(int i = 2; i <= n; i += 2) {
+        total += i * i;
+    }
+
+    cout << total;
+}
